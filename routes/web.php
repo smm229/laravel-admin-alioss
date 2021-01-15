a2a18b828a0fa1ff205aaf733f5c10c5506f2c20
yanxx <yanxiangxiang2997@dingtalk.com>
2021-01-15 09:38:15 +0800

    初始化

---
+++ routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use smm229\AdminOss\Http\Controllers\OssFormController;
+
+Route::get('alioss_param', OssFormController::class . '@getOssParam');
+
---
+++ src/AliOssForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace smm229\AdminOss;
+
+use Encore\Admin\Extension;
+
+class AliOssForm extends Extension
+{
+	public $name = 'laravel-admin-alioss';
+
+	public $views = __DIR__ . '/../resources/views';
+
+	public $assets = __DIR__ . '/../resources/assets';
+}
+
